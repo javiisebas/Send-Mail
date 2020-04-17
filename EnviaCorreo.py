c6ed65880a41ccc14c24cb2d6747469c70f839a2
Javier Sebastián Fernández <32978953+javiisebas@users.noreply.github.com>
2020-04-17 22:45:55 +0200

    Add files via upload

--- EnviaCorreo.py
+++ EnviaCorreo.py
@@ -1,183 +1,106 @@
-# Autor: Javier Sebastian Fernandez
-# Contacto: javi.sebas@hotmail.es
+# Author: Javier Sebastian Fernandez
+# Contact: javi.sebas@hotmail.es
 
 
-# Importamos las librerias necesarias
-
-import smtplib
+# We import all the needed libraries
 import sys 
 from getpass import getpass    
-from email.mime.text import MIMEText
-from tkinter import *
-from tkinter import scrolledtext
-from tkinter import messagebox
-from tkinter import Menu
-from tkinter import filedialog
-from tkinter import font
+
+from Functions import *
 
 
-# Creamos la ventana
+# We create de main window
 window = Tk() 
-window.title("Correo electrónico") 
-window.geometry('500x475')
+window.title("Mail Sender") 
+window.geometry('500x500')
 
 
-# -----------CAJAS-----------
-# Cajas en blanco
+# -----------------Labels-----------------
+# Boxes with no label
 col = [0,2,4,6,8,9,11]  
 for i in col:
     lbl = Label(window, text=" ") 
     lbl.grid(column=0, row=i)
    
-# Cajas con contenido   
-texto_cajas = ["Desde","Contraseña","Para","Asunto"] 
-numero_fila = 1
-for texto in texto_cajas:
-    lbl = Label(window, text="    " + texto + " :  ") 
-    lbl.grid(column=1, row=numero_fila,sticky = E)
-    numero_fila += 2
-# ----------------------------
+# Box labels  
+label_text = ["From","Password","To","Subject"] 
+row = 1
+for text in label_text:
+    lbl = Label(window, text="    " + text + " :  ") 
+    lbl.grid(column=1, row=row,sticky = E)
+    row += 2
+
+# Message box label
+lbl_msg = Label(window, text="         Message :") 
+lbl_msg.place(x=47,y=195)
+
+# Attach new file label
+lbl_attach = Label(window, text="Attached files: ")
+lbl_attach.place(x=250,y=404)
+# ----------------------------------------
     
-# Usuario
-emisor = Entry(window,width=59) 
-emisor.grid(column=2, row=1,sticky = W)
+# Mail of the user
+transmitter = Entry(window,width=59) 
+transmitter.grid(column=2, row=1,sticky = W)
 
-
-# Contraseña del usuario
+# Password of the user
 password = Entry(window,width=59,show="*")
 password.grid(column=2, row=3,sticky = W)
 
+# Mail of the receiver
+receiver = Entry(window,width=59) 
+receiver.grid(column=2, row=5,sticky = W)
 
-# Receptor del mensaje
-receptor = Entry(window,width=59) 
-receptor.grid(column=2, row=5,sticky = W)
+# Subject of the message
+subject = Entry(window,width=59) 
+subject.grid(column=2, row=7,sticky = W)
 
+# Message from the user
+text = scrolledtext.ScrolledText(window,width=51,height=9) 
+text.place(x=47,y=230)
 
-# Asunto del mensaje
-asunto = Entry(window,width=59) 
-asunto.grid(column=2, row=7,sticky = W)
+# Variables to attach the files
+fileList = []
+num_attached = 0
 
+# Button to attach new filws
+btn_attach = Button(window,text="  Attach file  ", command=newAttached)
+btn_attach.place(x=47,y=400)
 
-# Mensaje
-lbl4 = Label(window, text="         Mensaje :") 
-lbl4.place(x=47,y=195)
-texto = scrolledtext.ScrolledText(window,width=51,height=11) 
-texto.place(x=47,y=230)
+# Button to see the attached files
+btn_attached_list = Button(window,text=str(num_attached), command=showAttached)
+btn_attached_list.place(x=360,y=400)
+btn_attached_list["text"] = "   {}   ".format(str(num_attached))
 
+# Button to delete the attached files
+btn_attach = Button(window,text="  Delete  ", command=deleteAdj)
+btn_attach.place(x=405,y=400)
 
-# Distintos tipos de servidores
-def BuscaServidor(correo):
-    servidor = correo[correo.find("@")+1:][:correo[correo.find("@")+1:].find(".")]
-    print(servidor)
-    if servidor == "gmail" or servidor == "ucm":
-        return "smtp.gmail.com"
-    if servidor == "yahoo":
-        return "mail.yahoo.com"
-    if servidor == "hotmail" or servidor == "outlook":
-        return "smtp.live.com"
-
-
-# Enviado del mensaje
-def clicked(): 
-    global emisor,receptor,password,asunto
-    try:
-        mensaje = MIMEText(texto.get("1.0",END)) 
-        mensaje['From']=emisor.get()
-        mensaje['To']=receptor.get()
-        mensaje['Subject']=asunto.get()
-        
-        serverSMTP = smtplib.SMTP(BuscaServidor(emisor.get()),587) 
-        serverSMTP.ehlo() 
-        serverSMTP.starttls() 
-        serverSMTP.ehlo() 
-        serverSMTP.login(emisor.get(),password.get()) 
-
-        serverSMTP.sendmail(emisor.get(),receptor.get(),mensaje.as_string()) 
-        serverSMTP.close()
-        messagebox.showinfo('Información','Correo enviado')
-        
-        # Reseteamos la información del correo
-        texto.delete("1.0",END)
-        receptor.delete(0,END)
-        asunto.delete(0,END)
-        
-    except:
-        messagebox.showinfo('Información','Usuario o contraseña incorrectos')
-
-      
-# Botón de enviado
-btn = Button(window, text="ENVIAR", command=clicked, width=58, height=1, anchor="center")
-btn.place(x=46,y=427)
-
-
-
-# Creamos las funciones que posteriormente vamos a usar en el menú
-
-# Clase para generarme un mensaje
-class MsgBox(Toplevel):
-    def __init__(self, titulo=" ", mensaje=" "):
-        Toplevel.__init__(self)
-
-        self.title(titulo)
-        
-        self.font = font.Font(size=11)
-        self.label = Label(self, text=mensaje, font = self.font)
-        self.label['bg'] = 'white'
-        self.label.pack(ipadx=50, ipady=10, fill='both', expand=True)
-
-        self.button = Button(self, text="Cerrar")
-        self.button['command'] = self.destroy
-        self.button.pack(pady=10, padx=10, ipadx=20, side='left')
-
-# Genera un mensaje cuando es llamada
-def about():
-    mensaje = """
-    La aplicación ha sido creada
-    con el fin de poder enviar
-    correos de forma rápida y ágil.
-    
-    Pudiendo utilizar la misma 
-    aplicación para cualquier cuenta
-    de correo de gmail, hotmail y yahoo
-    """
-    msg = MsgBox("Información", mensaje)
-
-
-# Limpia las cajas cuando es llamada
-def limpiar():
-    texto.delete("1.0",END)
-    receptor.delete(0,END)
-    asunto.delete(0,END)
-    emisor.delete(0,END)
-    password.delete(0,END)
+# Button to send the mail
+btn = Button(window, text="SEND", command=clicked, width=58, height=1, anchor="center")
+btn.place(x=46,y=447)
   
-  
-# Cierra la ventana cuando es llamada  
-def salirAplicacion():
-    decision = messagebox.askquestion("Salir","Deseas salir de la aplicación?")
-    if decision == "yes":
-        window.destroy()
-        
 
-    
-# Creamos el MENÚ
-
+# We create the element menu
 menubar = Menu(window)
 
-# Creamos seccion "Configuración"
+# We create the section Configuration
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Borrar", command=limpiar)
+filemenu.add_command(label="Delete", command=clean)
 filemenu.add_separator()
-filemenu.add_command(label="Salir", command=salirAplicacion)
-menubar.add_cascade(label="Configuracion", menu=filemenu)
+filemenu.add_command(label="Exit", command=exitApp)
 
-# Creamos seccion "Ayuda"
+# We add the section to the menu
+menubar.add_cascade(label="Configuration", menu=filemenu)
+
+# We create the section Configuration
 helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="Información", command=about)
-menubar.add_cascade(label="Ayuda", menu=helpmenu)
+helpmenu.add_command(label="Information", command=about)
+
+# We add the section to the menu
+menubar.add_cascade(label="Help", menu=helpmenu)
 
 window.config(menu=menubar)
 
-# La ventana se mantendrá en un mainloop para que la interfaz esté siempre en ejecución
+# Mainloop to maintain the window working
 window.mainloop() 
-
